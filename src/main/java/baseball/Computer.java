33766408f04c35590a69049daf0ef2e229497475
saera <yook.saera@gmail.com>
2023-11-24 02:02:12 +0900

    feat(baseball): 컴퓨터 숫자와 사용자 숫자를 비교해 볼과 스트라이크 개수를 리턴하라

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import java.util.List;
+
+public class Computer {
+    private final List<Integer> numbers;
+
+    public Computer(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public List<Integer> compareTo(List<Integer> UserNumbers) {
+        int totalMatchNumbers = (int) UserNumbers.stream().filter(numbers::contains).count();
+        int strikeCount = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            if (UserNumbers.get(i).equals(numbers.get(i))) {
+                strikeCount++;
+            }
+        }
+        int ballCount = totalMatchNumbers - strikeCount;
+        return List.of(ballCount, strikeCount);
+    }
+}
