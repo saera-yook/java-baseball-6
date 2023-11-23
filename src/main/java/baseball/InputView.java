68ebc3547e4004102a3322f657332652d78aeb94
saera <yook.saera@gmail.com>
2023-11-24 04:30:12 +0900

    refactor(InputView): InputView에 구현된 함수 내에서 직접 사용자 입력을 받아라

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -3,19 +3,22 @@ package baseball;
 import static baseball.MessageContainer.ASK_RESTART;
 import static baseball.MessageContainer.ENTER_NUMBERS;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class InputView {
-    public List<Integer> readNumbers(String input) {
+    public List<Integer> readNumbers() {
         System.out.print(ENTER_NUMBERS);
+        String input = Console.readLine();
         Validator.check3EqualToLengthOf(input);
         Validator.checkIs1To9(input);
         Validator.checkDuplicateNumber(input);
         return Converter.convert(input);
     }
 
-    public String readChoice(String input) {
+    public String readChoice() {
         System.out.println(ASK_RESTART);
+        String input = Console.readLine();
         Validator.checkIs1Or2(input);
         return input;
     }
