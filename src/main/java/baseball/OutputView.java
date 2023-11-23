a4b84152ed128965466675a69d58d95b2865e5a8
saera <yook.saera@gmail.com>
2023-11-24 03:16:34 +0900

    feat(baseball): 게임 시작 문구를 출력하라

---
+++ src/main/java/baseball/OutputView.java
@@ -0,0 +1,9 @@
+package baseball;
+
+import static baseball.MessageContainer.WELCOME;
+
+public class OutputView {
+    public void printWelcomeMessage() {
+        System.out.println(WELCOME);
+    }
+}
