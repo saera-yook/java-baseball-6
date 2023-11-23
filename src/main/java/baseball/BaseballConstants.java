a625bdcec2d8ec13b45c55abdaea49bd980365a5
saera <yook.saera@gmail.com>
2023-11-24 04:32:34 +0900

    refactor(BaseballConstants): 입력 숫자 범위 상수들을 문자형으로 바꿔라

--- src/main/java/baseball/BaseballConstants.java
+++ src/main/java/baseball/BaseballConstants.java
@@ -2,8 +2,8 @@ package baseball;
 
 public class BaseballConstants {
     public static final int TOTAL_DIGITS = 3;
-    public static final int LOWER_BOUND_OF_DIGIT = 1;
-    public static final int UPPER_BOUND_OF_DIGIT = 9;
+    public static final char LOWER_BOUND_OF_DIGIT = '1';
+    public static final char UPPER_BOUND_OF_DIGIT = '9';
     public static final String RESTART_CONDITION = "1";
     public static final String END_CONDITION = "2";
 }
