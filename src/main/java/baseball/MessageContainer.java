5fbf6cdd1039b8fbb19d0bc629e4b509f1f01e54
saera <yook.saera@gmail.com>
2023-11-24 02:41:45 +0900

    feat(baseball): Umpire가 두 수의 비교 결과를 선언하라

--- src/main/java/baseball/MessageContainer.java
+++ src/main/java/baseball/MessageContainer.java
@@ -5,6 +5,7 @@ public class MessageContainer {
     public static final String ENTER_NUMBERS = "숫자를 입력해주세요 : ";
     public static final String BALL = "볼";
     public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
     public static final String END_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String ASK_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 }
---
+++ src/main/java/baseball/Umpire.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import static baseball.MessageContainer.BALL;
+import static baseball.MessageContainer.NOTHING;
+import static baseball.MessageContainer.STRIKE;
+
+import java.util.List;
+
+public class Umpire {
+    public String call(List<Integer> counts) {
+        if (counts.stream().allMatch(count -> count.equals(0))) {
+            return NOTHING;
+        }
+        if (counts.get(0).equals(0)) {
+            return counts.get(1) + STRIKE;
+        }
+        if (counts.get(1).equals(0)) {
+            return counts.get(0) + BALL;
+        }
+        return counts.get(0) + BALL + " " + counts.get(1) + STRIKE;
+    }
+}
