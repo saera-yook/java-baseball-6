9794109de2239cff9d599ad9f09b88c61fe78ca8
saera <yook.saera@gmail.com>
2023-11-23 15:23:00 +0900

    refactor(baseball): 문자형 상수를 정수형으로 바꿔라

--- src/main/java/baseball/BaseballConstants.java
+++ src/main/java/baseball/BaseballConstants.java
@@ -2,6 +2,6 @@ package baseball;
 
 public class BaseballConstants {
     public static final int TOTAL_DIGITS = 3;
-    public static final char LOWER_BOUND_OF_DIGIT = '1';
-    public static final char UPPER_BOUND_OF_DIGIT = '9';
+    public static final int LOWER_BOUND_OF_DIGIT = 1;
+    public static final int UPPER_BOUND_OF_DIGIT = 9;
 }
